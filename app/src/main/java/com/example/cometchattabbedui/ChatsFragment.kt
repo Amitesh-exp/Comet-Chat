d079c7c657871db8947b81c39d8a3e6076805df5
Amitesh-exp <amitesh2878@gmail.com>
2025-06-21 10:18:04 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/cometchattabbedui/ChatsFragment.kt
@@ -0,0 +1,57 @@
+package com.example.cometchattabbedui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.fragment.app.Fragment
+import com.cometchat.chat.constants.CometChatConstants
+import com.cometchat.chat.models.Conversation
+import com.cometchat.chat.models.Group
+import com.cometchat.chat.models.User
+import com.cometchat.chatuikit.conversations.CometChatConversations
+
+class ChatsFragment : Fragment() {
+
+    private lateinit var conversationsView: CometChatConversations
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_chats, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        setListeners()
+    }
+
+    private fun initView() {
+        conversationsView = requireView().findViewById(R.id.cometChatConversations)
+    }
+
+    private fun setListeners() {
+        conversationsView.setOnItemClick { _, _, conversation ->
+            startMessageActivity(conversation)
+        }
+    }
+
+    private fun startMessageActivity(conversation: Conversation) {
+        val intent = Intent(requireContext(), MessageActivity::class.java).apply {
+            when (conversation.conversationType) {
+                CometChatConstants.CONVERSATION_TYPE_GROUP -> {
+                    val group = conversation.conversationWith as Group
+                    putExtra("guid", group.guid)
+                }
+                else -> {
+                    val user = conversation.conversationWith as User
+                    putExtra("uid", user.uid)
+                }
+            }
+        }
+        startActivity(intent)
+    }
+}
