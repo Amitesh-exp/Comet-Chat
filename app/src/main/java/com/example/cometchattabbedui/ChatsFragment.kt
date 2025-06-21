ed8b40095915d9980cfb5a6f3466c3e68329f1db
Amitesh-exp <amitesh2878@gmail.com>
2025-06-21 14:54:29 +0530

    Initial Commit

--- app/src/main/java/com/example/cometchattabbedui/ChatsFragment.kt
+++ app/src/main/java/com/example/cometchattabbedui/ChatsFragment.kt
@@ -2,6 +2,7 @@ package com.example.cometchattabbedui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -40,15 +41,28 @@ class ChatsFragment : Fragment() {
     }
 
     private fun startMessageActivity(conversation: Conversation) {
+
+        Log.d("ChatsFragment", "Conversation clicked: ${conversation.conversationType}, with: ${conversation.conversationWith}")
+
         val intent = Intent(requireContext(), MessageActivity::class.java).apply {
             when (conversation.conversationType) {
                 CometChatConstants.CONVERSATION_TYPE_GROUP -> {
-                    val group = conversation.conversationWith as Group
-                    putExtra("guid", group.guid)
+                    val group = conversation.conversationWith as? Group
+                    if (group != null) {
+                        Log.d("ChatsFragment", "Starting group chat with ${group.guid}")
+                        putExtra("guid", group.guid)
+                    } else {
+                        Log.e("ChatsFragment", "conversationWith is not a Group")
+                    }
                 }
                 else -> {
                     val user = conversation.conversationWith as User
-                    putExtra("uid", user.uid)
+                    if (user != null) {
+                        Log.d("ChatsFragment", "Starting user chat with ${user.uid}")
+                        putExtra("uid", user.uid)
+                    } else {
+                        Log.e("ChatsFragment", "conversationWith is not a User")
+                    }
                 }
             }
         }
